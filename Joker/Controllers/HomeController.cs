6dab905dcfc9ecd1cb62ea17eba3e06cd6358915
user <user>
2015-05-28 16:45:42 +1200

    profile page started and hopefulyy working

--- Joker/Controllers/HomeController.cs
+++ Joker/Controllers/HomeController.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Joker.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Joker.Controllers
 {
     public class HomeController : Controller
     {
+        protected UserManager<ApplicationUser> UserManager { get; set; }
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        public HomeController()
+        {
+            this.UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(this.db));   
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -25,6 +36,14 @@ namespace Joker.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        [Authorize]
+        public ActionResult Profile()
+        {
+            
+            var currentUser = UserManager.FindById(User.Identity.GetUserId());
+            var jokes = currentUser.Jokes.ToList();
+            return View(jokes);
         }
     }
 }
